61b1c2f06ade21cbf2562381e9469de23ca12f87
Jayakrishnan J S <jayakrishnan.jkjs@gmail.com>
2025-12-14 23:18:32 +0530

    Corrected code: Implement a "catch-all" mechanism to intercept any unhandled exception and return a consistent, structured JSON response with an Internal Server Error

--- account-service/src/main/java/com/jkbank/accounts/exception/GlobalExceptionHandler.java
+++ account-service/src/main/java/com/jkbank/accounts/exception/GlobalExceptionHandler.java
@@ -1,19 +1,42 @@
 package com.jkbank.accounts.exception;
 
 import com.jkbank.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice // Indicates that this class provides global exception handling
-public class GlobalExceptionHandler{
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler { // extend ResponseEntityExceptionHandler to handle specific exceptions
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid( // handle validation errors for @Valid annotated request bodies
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> validationErrors = new HashMap<>(); // to store field-wise validation errors
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors(); // get all validation errors from exception object
+
+        validationErrorList.forEach((error) -> { // iterate through each validation error
+            String fieldName = ((FieldError)error).getField(); // get the field name that caused the validation error
+            String validationMsg = error.getDefaultMessage(); // get the default validation message
+            validationErrors.put(fieldName, validationMsg); // add field name and corresponding validation message to the map
+        });
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST); // return the map of validation errors with HTTP 400 status
+    }
 
     @ExceptionHandler(Exception.class) // catch and handle all generic exceptions including global runtime exceptions
-    public ResponseEntity<ErrorResponseDto> handleGlobalException(CustomerAlreadyExistsException exception, WebRequest webRequest){
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest webRequest){
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(
                 webRequest.getDescription(false),
                 HttpStatus.INTERNAL_SERVER_ERROR,
