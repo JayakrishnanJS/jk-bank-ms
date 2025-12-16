f0c1f52cb3d01b903c2476a84b08dea66e6e4529
Jayakrishnan J S <jayakrishnan.jkjs@gmail.com>
2025-12-16 22:37:51 +0530

    Created Loans microservice - complete implementation

--- loan-service/src/main/java/com/jkbank/loans/LoansApplication.java
+++ loan-service/src/main/java/com/jkbank/loans/LoansApplication.java
@@ -1,13 +1,39 @@
 package com.jkbank.loans;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans Service API Documentation",
+				description = " JK Bank - Loans Service REST API Documentation",
+				version = "1.0",
+				contact = @Contact(
+						name = "Jayakrishnan J S",
+						email = "jayakrishnan.jkjs@gmail.com",
+						url = "https://www.linkedin.com/in/jayakrishnan-j-s-6a5167171/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+				)
+		),
+		externalDocs =  @ExternalDocumentation(
+				description = "JK Bank - Loans Service Documentation",
+				url = "https://www.linkedin.com/in/jayakrishnan-j-s-6a5167171/"
+		)
+)
 public class LoansApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoansApplication.class, args);
 	}
-
 }
