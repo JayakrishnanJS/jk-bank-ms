8dec52c53a58591e01fa3184f91dbaa598d05df8
Jayakrishnan J S <jayakrishnan.jkjs@gmail.com>
2025-12-19 00:14:47 +0530

    Refresh configurations at runtime using refresh actuator path

--- account-service/src/main/java/com/jkbank/accounts/dto/AccountsContactInfoDto.java
+++ account-service/src/main/java/com/jkbank/accounts/dto/AccountsContactInfoDto.java
@@ -1,5 +1,7 @@
 package com.jkbank.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -7,7 +9,10 @@ import java.util.Map;
 
 // record class is used to hold immutable data objects with less boilerplate code
 @ConfigurationProperties(prefix = "accounts") // to bind properties as in application.yml
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails,
-                                     List<String> onCallSupport) {
-
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+ private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
