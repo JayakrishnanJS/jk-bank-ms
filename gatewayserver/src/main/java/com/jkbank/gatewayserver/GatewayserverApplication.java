6f51d64365f2bb0fb85b49af3e06960c1744c3d6
Jayakrishnan J S <jayakrishnan.jkjs@gmail.com>
2025-12-23 00:02:17 +0530

    gatewayserver: Implemented per-route Http timeout configuration for loans ms

--- gatewayserver/src/main/java/com/jkbank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/jkbank/gatewayserver/GatewayserverApplication.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
 
+import static org.springframework.cloud.gateway.support.RouteMetadataUtils.CONNECT_TIMEOUT_ATTR;
+import static org.springframework.cloud.gateway.support.RouteMetadataUtils.RESPONSE_TIMEOUT_ATTR;
+
 @SpringBootApplication
 public class GatewayserverApplication {
 
@@ -31,7 +34,11 @@ public class GatewayserverApplication {
                         .path("/jkbank/loans/**")
                         .filters(f -> f.rewritePath("/jkbank/loans/(?<segment>.*)", "/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-                        .uri("lb://LOANS"))
+                        .metadata(RESPONSE_TIMEOUT_ATTR, 5000) // per-route response timeout
+                        .metadata(CONNECT_TIMEOUT_ATTR, 1000) // per-route connection timeout
+                        // similar to httpclient in application.yml
+                        .uri("lb://LOANS")
+                )
                 .route(p -> p
                         .path("/jkbank/cards/**")
                         .filters(f -> f.rewritePath("/jkbank/cards/(?<segment>.*)", "/${segment}")
