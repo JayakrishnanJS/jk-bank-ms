d95eca56e62af4099fd51884027ecc8bbcbb5d46
Jayakrishnan J S <jayakrishnan.jkjs@gmail.com>
2025-12-23 01:12:00 +0530

    gatewayserver: implemented Resilience4J Circuit Breaker configuration used to set timeouts for all circuit breakers created in the ReactiveResilience4JCircuitBreakerFactory(can also configure in app.yml file) -> this may avoid timeout of CB between retries

--- gatewayserver/src/main/java/com/jkbank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/jkbank/gatewayserver/GatewayserverApplication.java
@@ -1,12 +1,17 @@
 package com.jkbank.gatewayserver;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
@@ -52,5 +57,15 @@ public class GatewayserverApplication {
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://CARDS"))
                 .build();
+    }
+
+    // Resilience4J Circuit Breaker configuration used to set timeouts for all circuit breakers created in the factory
+    // -> this may avoid timeout of CB between retries
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+        // All circuit breakers created by this factory will have a timeout of 4 seconds
     }
 }
