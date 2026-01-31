7c2db8a4c5d69498b0d62b1a899880f433fa8869
JayakrishnanJS <jayakrishnan.jkjs@gmail.com>
2026-01-31 23:32:36 +0530

    account-service,loan-service, message-service: changed from rabbitmq to kafka, added loan service to msg service async communication

--- loan-service/src/main/java/com/jkbank/loans/service/impl/LoansServiceImpl.java
+++ loan-service/src/main/java/com/jkbank/loans/service/impl/LoansServiceImpl.java
@@ -1,14 +1,21 @@
 package com.jkbank.loans.service.impl;
 
 import com.jkbank.loans.constants.LoansConstants;
+import com.jkbank.loans.dto.CustomerAccountDto;
 import com.jkbank.loans.dto.LoansDto;
+import com.jkbank.loans.dto.LoansMsgDto;
 import com.jkbank.loans.entity.Loans;
 import com.jkbank.loans.exception.LoanAlreadyExistsException;
 import com.jkbank.loans.exception.ResourceNotFoundException;
 import com.jkbank.loans.mapper.LoansMapper;
 import com.jkbank.loans.repository.LoansRepository;
 import com.jkbank.loans.service.ILoansService;
+import com.jkbank.loans.service.client.AccountsFeignClient;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -18,18 +25,57 @@ import java.util.Random;
 @AllArgsConstructor
 public class LoansServiceImpl implements ILoansService {
 
+    private static final Logger log = LoggerFactory.getLogger(LoansServiceImpl.class);
+
     private LoansRepository loansRepository;
+
+    private AccountsFeignClient accountsFeignClient;
+
+    private final StreamBridge streamBridge; // autowired - to publish events to Kafka topics
 
     /**
      * @param mobileNumber - Mobile Number of the Customer
      */
     @Override
-    public void createLoan(String mobileNumber) {
+    public void createLoan(String mobileNumber, String correlationId) {
         Optional<Loans> optionalLoans= loansRepository.findByMobileNumber(mobileNumber);
         if(optionalLoans.isPresent()){
             throw new LoanAlreadyExistsException("Loan already registered with given mobileNumber "+mobileNumber);
         }
-        loansRepository.save(createNewLoan(mobileNumber));
+
+        Loans loan = loansRepository.save(createNewLoan(mobileNumber));
+        LoansMapper.mapToLoansDto(loan, new LoansDto());
+
+        // Fetch customer account details using AccountsFeignClient
+        ResponseEntity<CustomerAccountDto> customerAccountDtoResponseEntity = accountsFeignClient.fetchCustomerAccountDetails(correlationId, mobileNumber);
+        System.out.println("customerAccountDtoResponseEntity: " + customerAccountDtoResponseEntity);
+        log.info("customerAccountDtoResponseEntity: {}", customerAccountDtoResponseEntity.getBody());
+        if(customerAccountDtoResponseEntity.getBody() != null) {
+            CustomerAccountDto customerAccountDto = customerAccountDtoResponseEntity.getBody();
+
+            // Trigger communication event after successful loan creation
+            sendCommunication(loan, customerAccountDto);
+        }
+    }
+
+    private void sendCommunication(Loans loan, CustomerAccountDto customerAccountDto) {
+        try {
+            var loansMsgDto = new LoansMsgDto(
+                    customerAccountDto.getName(),
+                    customerAccountDto.getAccountNumber(),
+                    customerAccountDto.getAccountType(),
+                    customerAccountDto.getMobileNumber(),
+                    loan.getLoanNumber(),
+                    loan.getLoanType(),
+                    loan.getTotalLoan()
+            ); // var -> reduce boilerplate when the type is obvious from the right-hand side
+            log.info("Sending Communication request for loan: {}", loan.getLoanNumber());
+            var result = streamBridge.send("sendCommunication-out-0", loansMsgDto);
+            log.info("Communication request triggered: {}", result);
+        } catch (Exception e) {
+            log.error("Failed to send communication for loan: {}", loan.getLoanNumber(), e);
+            // Don't throw exception - loan already created, communication is secondary
+        }
     }
 
     /**
@@ -88,5 +134,23 @@ public class LoansServiceImpl implements ILoansService {
         return true;
     }
 
-
+    /**
+     * Update communication status for the given account number.
+     *
+     * @param loanNumber the account number of the customer
+     * @return true if the update was successful, false otherwise
+     */
+    @Override
+    public boolean updateCommunicationStatus(Long loanNumber) {
+        boolean isUpdated = false;
+        if(loanNumber != null) {
+            Loans loans = loansRepository.findByLoanNumber(loanNumber.toString()).orElseThrow(
+                    () -> new ResourceNotFoundException("Loan", "LoanNumber", loanNumber.toString())
+            );
+            loans.setCommunicationSwitch(true);
+            loansRepository.save(loans);
+            isUpdated = true;
+        }
+        return isUpdated;
+    }
 }
--- message/src/main/java/com/jkbank/message/functions/MessageFunctions.java
+++ message/src/main/java/com/jkbank/message/functions/MessageFunctions.java
@@ -1,6 +1,7 @@
 package com.jkbank.message.functions;
 
 import com.jkbank.message.dto.AccountsMsgDto;
+import com.jkbank.message.dto.LoansMsgDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +15,7 @@ public class MessageFunctions {
     private static final Logger log = LoggerFactory.getLogger(MessageFunctions.class.getName());
 
     @Bean
-    public Function<AccountsMsgDto, AccountsMsgDto> email(){
+    public Function<AccountsMsgDto, AccountsMsgDto> emailAccountDetails(){
         return accountsMsgDto -> {
           log.info("Sending email with the details: "+accountsMsgDto.toString());
           return accountsMsgDto;
@@ -22,11 +23,28 @@ public class MessageFunctions {
     }
 
     @Bean
-    public Function<AccountsMsgDto, Long> sms(){
+    public Function<AccountsMsgDto, Long> smsAccountDetails(){
         return accountsMsgDto -> {
             log.info("Sending sms with the details: "+accountsMsgDto.toString());
             return accountsMsgDto.accountNumber();
         };
     }
 
+    // --- Loans Functions ---
+    @Bean
+    public Function<LoansMsgDto, LoansMsgDto> emailLoanDetails() {
+        return loansMsgDto -> {
+            log.info("Sending email with the LOAN details: {}", loansMsgDto);
+            return loansMsgDto;
+        };
+    }
+
+    @Bean
+    public Function<LoansMsgDto, Long> smsLoanDetails() {
+        return loansMsgDto -> {
+            log.info("Sending SMS with the LOAN details: {}", loansMsgDto);
+            return Long.valueOf(loansMsgDto.loanNumber());
+        };
+    }
 }
+
